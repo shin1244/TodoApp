54edca99707ee4f8adecf49cca92569bad488f6b
shin1244 <tlswnsrb1357@gmail.com>
2025-03-27 18:07:48 +0900

    ToDo앱 제작 시작

--- App.tsx
+++ App.tsx
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { theme } from './colors';
+import { useState } from 'react';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({})
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload:string) => setText(payload)
+  const addToDo = () => {
+    if(text === "") {
+      return;
+    } 
+    const newToDos = Object.assign({}, toDos, {[Date.now()]: {text, work:working}})
+    setToDos(newToDos)
+    setText("");
+  }
+  console.log(toDos)
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text style={{...styles.btnText, color:working?"white":theme.grey}}>Work</Text>
+          </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{...styles.btnText, color:!working?"white":theme.grey}}>Travel</Text>
+          </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput 
+        placeholder={working ? "Add a To Do":"Where do you want to go?"} 
+        style={styles.input}
+        value={text}
+        onChangeText={onChangeText}
+        onSubmitEditing={addToDo}></TextInput>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    backgroundColor:theme.bg,
+    paddingHorizontal: 20,
   },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38 ,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical:15,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    marginTop:20,
+    fontSize: 18,
+  }
 });
